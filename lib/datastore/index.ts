7f4829a1b2106e25073a8822048656785a64d448
Alex <alexandru.pausan@gmail.com>
2023-08-01 14:22:08 +0300

    Added back the file processing functionality

--- lib/datastore/index.ts
+++ lib/datastore/index.ts
@@ -9,26 +9,20 @@ import { uploadToS3 } from '@/lib/datastore/s3'
 const TEST_COLLECTION = 'test-collection'
 
 export const uploadFile = async (fileBlob: Blob) => {
-  // uploadToS3(fileBlob)
-
+  uploadToS3(fileBlob)
   // @TODO: return file upload success, and run the rest of the process in the background
 
   const docs = await getDocumentChunks(fileBlob)
 
-  return docs
   if (!docs.length) {
     console.log('No docs')
     return
   }
-  console.log('DOCS', docs)
 
-  console.log('vectors')
-  // console.log('DOCS', docs)
-  // TODO: return the
   const result = await createAndStoreVectors(docs)
 
   console.log(result)
-  return result
+  return docs
 }
 
 export const searchSimilarText = async (message: string, collection: string = TEST_COLLECTION) => {
