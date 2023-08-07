da6f3b0bedece65780d9aed1ea9cadd3454fbdb7
Alex <alexandru.pausan@gmail.com>
2023-08-07 14:31:59 +0200

    Added separate route fo rthe demo chat

---
+++ app/api/demo-chat/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server'
+import { AIMessage, HumanMessage } from 'langchain/schema'
+import { StreamingTextResponse, LangChainStream, Message } from 'ai'
+
+import { getOpenAIConnection } from '@/lib/openAI'
+
+export async function POST(req: Request, res: NextResponse) {
+  const { messages } = await req.json()
+
+  if (!messages) {
+    return new Response('No message provided', {
+      status: 400,
+    })
+  }
+
+  const { stream, handlers } = LangChainStream()
+  const chat = getOpenAIConnection()
+
+  chat
+    .call(
+      (messages as Message[]).map(({ role, content }) =>
+        role == 'user' ? new HumanMessage(content) : new AIMessage(content),
+      ),
+      {},
+      [handlers],
+    )
+    .catch(console.error)
+
+  return new StreamingTextResponse(stream)
+}
