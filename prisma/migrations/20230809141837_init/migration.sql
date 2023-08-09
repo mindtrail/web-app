1397453ee534f752f7c4791b1aaaea8c7651434c
Alex <alexandru.pausan@gmail.com>
2023-08-09 16:18:49 +0200

    Updated migration

---
+++ prisma/migrations/20230809141837_init/migration.sql
@@ -0,0 +1,276 @@
+-- CreateEnum
+CREATE TYPE "DataStoreVisibility" AS ENUM ('public', 'private');
+
+-- CreateEnum
+CREATE TYPE "DataSourceStatus" AS ENUM ('unsynched', 'pending', 'running', 'synched', 'error', 'usage_limit_reached');
+
+-- CreateEnum
+CREATE TYPE "DataSourceType" AS ENUM ('web_page', 'web_site', 'text', 'file', 'google_drive_file', 'google_drive_folder', 'notion');
+
+-- CreateEnum
+CREATE TYPE "DataStoreType" AS ENUM ('qdrant');
+
+-- CreateEnum
+CREATE TYPE "SubscriptionPlan" AS ENUM ('level_0', 'level_1', 'level_2', 'level_3');
+
+-- CreateEnum
+CREATE TYPE "PriceType" AS ENUM ('recurring');
+
+-- CreateEnum
+CREATE TYPE "SubscriptionStatus" AS ENUM ('active', 'canceled', 'incomplete', 'incomplete_expired', 'past_due', 'trialing', 'unpaid');
+
+-- CreateEnum
+CREATE TYPE "PriceInterval" AS ENUM ('day', 'month', 'week', 'year');
+
+-- CreateEnum
+CREATE TYPE "PromptType" AS ENUM ('raw', 'customer_support');
+
+-- CreateEnum
+CREATE TYPE "MessageFrom" AS ENUM ('agent', 'human');
+
+-- CreateEnum
+CREATE TYPE "ConversationChannel" AS ENUM ('dashboard', 'website', 'slack', 'crisp');
+
+-- CreateEnum
+CREATE TYPE "ModelName" AS ENUM ('gpt_3_5_turbo', 'gpt_3_5_turbo_16k', 'gpt_4', 'gpt_4_32k');
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+    "picture" TEXT,
+    "hasOptInEmail" BOOLEAN DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Usage" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "nbAgentQueries" INTEGER NOT NULL DEFAULT 0,
+    "nbDataStoreQueries" INTEGER NOT NULL DEFAULT 0,
+    "nbUploadedBytes" INTEGER NOT NULL DEFAULT 0,
+    "nbDataProcessingBytes" INTEGER NOT NULL DEFAULT 0,
+    "nbModelTokens" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Usage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Datatore" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "type" "DataStoreType" NOT NULL,
+    "visibility" "DataStoreVisibility" NOT NULL DEFAULT 'private',
+    "config" JSONB,
+    "ownerId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Datatore_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AppDataSource" (
+    "id" TEXT NOT NULL,
+    "type" "DataSourceType" NOT NULL,
+    "name" TEXT NOT NULL,
+    "status" "DataSourceStatus" NOT NULL DEFAULT 'unsynched',
+    "config" JSONB,
+    "dataStoreId" TEXT,
+    "ownerId" TEXT,
+    "nbChunks" INTEGER DEFAULT 0,
+    "textSize" INTEGER DEFAULT 0,
+    "hash" TEXT,
+    "nbSynch" INTEGER DEFAULT 0,
+    "lastSynch" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AppDataSource_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Conversation" (
+    "id" TEXT NOT NULL,
+    "visitorId" TEXT,
+    "userId" TEXT,
+    "agentId" TEXT,
+    "channel" "ConversationChannel" NOT NULL DEFAULT 'dashboard',
+    "metadata" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Conversation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "from" "MessageFrom" NOT NULL,
+    "conversationId" TEXT,
+    "sources" JSONB,
+    "read" BOOLEAN DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT false,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "image" TEXT,
+    "metadata" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Price" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT false,
+    "currency" TEXT NOT NULL,
+    "interval" "PriceInterval",
+    "unitAmount" INTEGER,
+    "interval_count" INTEGER,
+    "trial_period_days" INTEGER,
+    "type" "PriceType",
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Price_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Subscription" (
+    "id" TEXT NOT NULL,
+    "plan" "SubscriptionPlan" DEFAULT 'level_1',
+    "priceId" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "status" "SubscriptionStatus" NOT NULL,
+    "start_date" TIMESTAMP(3),
+    "ended_at" TIMESTAMP(3),
+    "trial_end" TIMESTAMP(3),
+    "trial_start" TIMESTAMP(3),
+    "cancel_at" TIMESTAMP(3),
+    "cancel_at_period_end" BOOLEAN,
+    "canceled_at" TIMESTAMP(3),
+    "metadata" JSONB,
+    "coupon" TEXT,
+    "userId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usage_userId_key" ON "Usage"("userId");
+
+-- CreateIndex
+CREATE INDEX "Conversation_visitorId_idx" ON "Conversation" USING HASH ("visitorId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subscription_userId_key" ON "Subscription"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Usage" ADD CONSTRAINT "Usage_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Datatore" ADD CONSTRAINT "Datatore_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AppDataSource" ADD CONSTRAINT "AppDataSource_dataStoreId_fkey" FOREIGN KEY ("dataStoreId") REFERENCES "Datatore"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AppDataSource" ADD CONSTRAINT "AppDataSource_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Conversation" ADD CONSTRAINT "Conversation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "Conversation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Price" ADD CONSTRAINT "Price_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_priceId_fkey" FOREIGN KEY ("priceId") REFERENCES "Price"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
