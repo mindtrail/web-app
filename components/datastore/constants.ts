ec1b8b907d92055538f173bee445e656097aa845
Alex <alexandru.pausan@gmail.com>
2023-08-10 16:20:00 +0200

    Added dropzone in the uI

--- components/datastore/constants.ts
+++ components/datastore/constants.ts
@@ -1,7 +1,6 @@
 export const ACCEPTED_FILE_TYPES = [
   'application/epub+zip',
   'application/json',
-  'application/octet-stream',
   'application/pdf',
   'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
   'application/x-ndjson',
@@ -11,6 +10,18 @@ export const ACCEPTED_FILE_TYPES = [
   'text/plain',
   'text/markdown',
 ]
+
+export const ACCEPTED_FILE_REACT_DROPZONE = {
+  'application/json': ['.json', '.jsonl'],
+  'application/pdf': ['.pdf'],
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document': ['.docx'],
+  'application/x-ndjson': ['.jsonl'],
+  'application/x-subrip': ['.srt'],
+  // 'application/octet-stream': ['', ''],
+  'text/csv': ['.csv'],
+  'text/plain': ['.txt', '.text', '.log'],
+  'text/markdown': ['.md'],
+}
 
 export const UPLOAD_ENDPOINT = '/api/upload'
 export const MAX_NR_OF_FILES = 30
