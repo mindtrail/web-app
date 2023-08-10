ff14347eecb6489731c3cb3e18a9aac32fb25a29
Alex <alexandru.pausan@gmail.com>
2023-08-10 18:48:09 +0200

    Updated styles for drag accept and reject

--- components/datastore/index.tsx
+++ components/datastore/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, MouseEventHandler } from 'react'
+import { useState, useEffect, useMemo, MouseEventHandler } from 'react'
 import { useDropzone } from 'react-dropzone'
 
 import { IconSpinner } from '@/components/ui/icons'
@@ -17,11 +17,15 @@ interface DataStoreProps extends React.ComponentProps<'div'> {
   dataStoreId: string
 }
 
+const defaultStyles = 'border-neutral-300 bg-neutral-50'
+const rejectStyles = 'border-red-400 bg-neutral-300 cursor-not-allowed text-neutral-800'
+const acceptStyles = 'border-green-600 bg-green-50'
+
 export function CreateDataStore({ dataStoreId }: DataStoreProps) {
   const [files, setFiles] = useState([])
   const [isInitializing, setIsInitializing] = useState(true)
 
-  const { getRootProps, getInputProps } = useDropzone({
+  const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } = useDropzone({
     accept: ACCEPTED_FILE_REACT_DROPZONE,
     // onDrop: acceptedFiles => {
     //   setFiles(acceptedFiles.map(file => Object.assign(file, {
@@ -48,18 +52,25 @@ export function CreateDataStore({ dataStoreId }: DataStoreProps) {
       <div className='max-w-lg w-full flex-1 relative flex flex-col gap-8'>
         <div
           {...getRootProps()}
-          className='flex flex-col w-full py-6 rounded-xl justify-center
-          border border-neutral-300 bg-neutral-50 items-center gap-4 text-neutral-600
-          select-none cursor-default'
+          className={`flex flex-col w-full h-28 rounded-xl justify-center
+          border items-center gap-4 text-neutral-600
+          select-none cursor-default transition .25s ease-in-out
+          ${isDragReject ? rejectStyles : isDragAccept ? acceptStyles : defaultStyles}`}
         >
           <input {...getInputProps()} />
-          <p>
-            Drag and drop or <span className='underline text-neutral-500'>Click</span> to select
-            files
-          </p>
-          <p className='text-sm text-neutral-500'>
-            <span>Supported file types:</span> .pdf, .docx, .txt, .md, .json, .jsonl, .csv
-          </p>
+          {isDragReject ? (
+            'Unsuported file type!'
+          ) : (
+            <>
+              <p>
+                Drag and drop or <span className='underline text-neutral-500'>Click</span> to select
+                files
+              </p>
+              <p className='text-sm text-neutral-500'>
+                <span>Supported file types:</span> .pdf, .docx, .txt, .md, .json, .jsonl, .csv
+              </p>
+            </>
+          )}
         </div>
         {fileList}
       </div>
