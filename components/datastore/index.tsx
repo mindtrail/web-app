0e64ca2daa701ed5833d75a9c8827caf2fddde6c
Alex <alexandru.pausan@gmail.com>
2023-08-14 10:55:27 +0200

    Added rhf

--- components/datastore/index.tsx
+++ components/datastore/index.tsx
@@ -89,10 +89,6 @@ export function CreateDataStore({ dataStoreId }: DataStoreProps) {
       <div className='max-w-lg w-full'>
         <DataStoreForm />
       </div>
-
-      <Button variant='default' size='wide' onClick={handleSubmit}>
-        Continue
-      </Button>
     </div>
   )
 }
