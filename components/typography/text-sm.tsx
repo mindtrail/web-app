1af02cf8436d651243e0161aa3b20cc569587cb6
Alex <alexandru.pausan@gmail.com>
2023-08-16 13:17:44 +0200

    Refactored typography

--- components/typography/text-sm.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-
-type textType = {
-  children: JSX.Element | ReactNode
-}
-
-export function TypographySmall({ children }: textType) {
-  return <small className='text-sm font-medium leading-none'>{children}</small>
-}
