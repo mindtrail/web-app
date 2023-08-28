6fa7be0de593739a3bdc731a4e0dcff68f9d8252
Alex <alexandru.pausan@gmail.com>
2023-08-28 12:45:11 +0300

    Commented out AI package

--- app/api/demo-chat/route.ts
+++ app/api/demo-chat/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import { AIMessage, HumanMessage } from 'langchain/schema'
-import { StreamingTextResponse, LangChainStream, Message } from 'ai'
+// import { StreamingTextResponse, LangChainStream, Message } from 'ai'
 
 import { getOpenAIConnection } from '@/lib/openAI'
 
@@ -13,18 +13,18 @@ export async function POST(req: Request, res: NextResponse) {
     })
   }
 
-  const { stream, handlers } = LangChainStream()
+  // const { stream, handlers } = LangChainStream()
   const chat = getOpenAIConnection()
 
-  chat
-    .call(
-      (messages as Message[]).map(({ role, content }) =>
-        role == 'user' ? new HumanMessage(content) : new AIMessage(content),
-      ),
-      {},
-      [handlers],
-    )
-    .catch(console.error)
+  // chat
+    // .call(
+      // (messages as Message[]).map(({ role, content }) =>
+        // role == 'user' ? new HumanMessage(content) : new AIMessage(content),
+      // ),
+      // {},
+      // [handlers],
+    // )
+    // .catch(console.error)
 
-  return new StreamingTextResponse(stream)
+  // return new StreamingTextResponse(stream)
 }
