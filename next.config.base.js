0640381735f44174f25629a267ac8e296c760a6e
Alex <alexandru.pausan@gmail.com>
2023-09-19 15:43:39 +0300

    Added next.config for docker

---
+++ next.config.base.js
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+// @TODO: add "output: 'standalone'," to nextConfig if I building a Docker image
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push({
+        bufferutil: 'bufferutil',
+        'utf-8-validate': 'utf-8-validate',
+        'supports-color': 'supports-color',
+      })
+    }
+
+    return config
+  },
+}
+process.on('unhandledRejection', (error) => {
+  console.log('unhandledRejection', error)
+})
+
+module.exports = nextConfig
