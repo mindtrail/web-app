f02ccb1da1f77e6d11fa1ee70e31534b7e41ed76
Alex <alexandru.pausan@gmail.com>
2023-09-21 17:53:43 +0300

    Fixed ts errors

--- app/api/embed/route.ts
+++ app/api/embed/route.ts
@@ -1,9 +1,6 @@
 import { NextResponse } from 'next/server'
-import { DataSrcType, DataSrcStatus } from '@prisma/client'
-
-import { uploadToGCS } from '@/lib/cloudStorage'
-import { createDataSrc, updateDataSrc } from '@/lib/db/dataSource'
-import { createAndStoreVectors } from '@/lib/qdrant'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '@/lib/authOptions'
 
 const EMBEDDING_SECRET = process.env.EMBEDDING_SECRET || ''
 
@@ -16,6 +13,16 @@ interface EmbeddingPayload {
 export async function POST(req: Request) {
   // This will be a call from a Cloud Run service, from the same project & VPC
   // I want to make this call accessible only from the Cloud Run service
+
+  const session = (await getServerSession(authOptions)) as ExtendedSession
+
+  const userId = session?.user?.id
+  if (!userId) {
+    return new NextResponse('Unauthorized', {
+      status: 401,
+    })
+  }
+
   const secret = req.headers.get('X-Custom-Secret')
   if (secret !== EMBEDDING_SECRET) {
     return new Response('Unauthorized', {
@@ -33,7 +40,6 @@ export async function POST(req: Request) {
       status: 400,
     })
   }
-  const { userId, dataStoreId, urls } = body
 
   // Return nr of chunks & character count
   const docs = {} // await getChunksFromFile(fileBlob)
@@ -47,41 +53,4 @@ export async function POST(req: Request) {
   }
 
   return NextResponse.json({ 1234: '1234' })
-
-  const nbChunks = docs.length
-  const textSize = docs.reduce((acc, doc) => acc + doc?.pageContent?.length, 0)
-
-  const dataSrcPayload = {
-    name: fileName,
-    dataStoreId,
-    ownerId: userId,
-    type: DataSrcType.file,
-    nbChunks,
-    textSize,
-  }
-
-  const dataSrc = await createDataSrc(dataSrcPayload)
-  const dataSrcId = dataSrc?.id
-
-  if (!dataSrcId) {
-    return new Response(`Failed to save File. Try again`, {
-      status: 400,
-    })
-  }
-
-  createAndStoreVectors({ docs, userId, dataStoreId, dataSrcId })
-
-  // Upload file to S3
-  const s3Upload = uploadToGCS({ fileBlob, userId, dataStoreId, dataSrcId })
-  // @TODO: return file upload success, and run the rest of the process in the background
-  s3Upload
-    .then((res) => {
-      updateDataSrc({ id: dataSrcId, status: DataSrcStatus.synched })
-    })
-    .catch((err) => {
-      updateDataSrc({ id: dataSrcId, status: DataSrcStatus.error })
-      console.error(err)
-    })
-
-  return NextResponse.json({ nbChunks, textSize, docs })
 }
