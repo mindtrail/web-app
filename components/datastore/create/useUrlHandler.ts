d9fb26cd1e58ae2101799d59eedfddf5b89da890
Alex <alexandru.pausan@gmail.com>
2023-10-01 19:10:10 +0300

    Added complete flow to add and delete URLs. Needs refactoring!

--- components/datastore/create/useUrlHandler.ts
+++ components/datastore/create/useUrlHandler.ts
@@ -1,8 +1,15 @@
 import { useState, useCallback } from 'react'
+import { DataSrc } from '@prisma/client'
+import { deleteFileApiCall } from '@/lib/api/dataStore'
+import { useToast } from '@/components/ui/use-toast'
 
 export function useUrlHandler(initialUrls: URLScrapped[] = []) {
   const [urls, setUrls] = useState(initialUrls)
   const [autoCrawl, setAutoCrawl] = useState(true)
+  const [urlToDelete, setUrlToDelete] = useState<URLScrapped | null>(null)
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false)
+
+  const { toast } = useToast()
 
   const handleUrlSubmit = useCallback(async (url: URLScrapped) => {
     try {
@@ -19,10 +26,63 @@ export function useUrlHandler(initialUrls: URLScrapped[] = []) {
     setAutoCrawl((prevAutoCrawl) => !prevAutoCrawl)
   }, [])
 
+  const handleURLDelete: DeleteHandler = useCallback((event, file) => {
+    event.preventDefault()
+    console.log('file', file)
+
+    if (!file) {
+      return
+    }
+
+    setUrlToDelete(file as URLScrapped)
+    setDeleteDialogOpen(true)
+  }, [])
+
+  const confirmURLDelete = useCallback(async () => {
+    if (!urlToDelete) {
+      return
+    }
+
+    try {
+      const { id, name } = urlToDelete.file as DataSrc
+      const deleteResult = await deleteFileApiCall(id)
+
+      toast({
+        title: 'Webpage deleted',
+        description: `${name} has been deleted`,
+      })
+      console.log(deleteResult)
+    } catch (err) {
+      toast({
+        title: 'Error',
+        variant: 'destructive',
+        description: `Something went wrong while deleting ${name}`,
+      })
+      console.log(err)
+    }
+
+    filterOutDeletedURLs(urlToDelete)
+  }, [urlToDelete, toast])
+
+  const filterOutDeletedURLs = (fileToDelete: URLScrapped) => {
+    const { file } = fileToDelete
+
+    setUrls((prevURLs) =>
+      prevURLs.filter(({ file: prevFile }) => {
+        return prevFile.id !== file.id
+      }),
+    )
+  }
+
   return {
     urls,
     autoCrawl,
+    urlToDelete,
+    deleteURLOpen: deleteDialogOpen,
     handleUrlSubmit,
     setAutoCrawl,
+    handleURLDelete,
+    confirmURLDelete,
+    setDeleteURLOpen: setDeleteDialogOpen,
   }
 }
