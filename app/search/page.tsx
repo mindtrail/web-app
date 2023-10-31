c00d1658c475a691a9f6e61c19d0cd51308b8c5d
Alex <alexandru.pausan@gmail.com>
2023-10-31 16:58:07 +0200

    Refactor search functionality and remove unnecessary console logs

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,10 +3,8 @@ import { redirect } from 'next/navigation'
 import { getServerSession } from 'next-auth/next'
 import { Session } from 'next-auth'
 
-import { searchHistory } from '@/lib/search'
 import { Search } from '@/components/search'
 import { authOptions } from '@/lib/authOptions'
-import { getDataSrcList } from '@/lib/db/dataSrc'
 
 interface UserWithId {
   id: string | null
@@ -26,15 +24,6 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function ChatPage() {
-  async function submitSearch(searchQuery: string) {
-    'use server'
-    const result = await searchHistory(searchQuery)
-    console.log(1111, searchQuery)
-    const response = await result.json()
-    console.log(2222, response)
-    // return result
-  }
-
   const session = (await getServerSession(authOptions)) as ExtSession
 
   if (!session?.user?.id) {
@@ -42,12 +31,9 @@ export default async function ChatPage() {
   }
 
   const userId = session.user.id
-  const historyItems = await getDataSrcList(userId)
-  console.log(222)
-  // return <div>Chat Page</div>
   return (
     <>
-      <Search foundWebsite={historyItems} submitSearch={submitSearch} />
+      <Search userId={userId} />
     </>
   )
 }
