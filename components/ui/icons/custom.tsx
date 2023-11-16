7802f4bfe8923f670abb75c7b9d9acfc937139cf
Alex <alexandru.pausan@gmail.com>
2023-11-16 16:07:48 +0200

    Rename "mindtrail" icon to "custom"

--- components/ui/icons/custom.tsx
+++ components/ui/icons/custom.tsx
@@ -15,27 +15,27 @@ function FilterIcon({ className, ...props }: React.ComponentProps<'svg'>) {
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <path d='M2.5 1.5H12.5' stroke='black' stroke-linecap='round' />
+      <path d='M2.5 1.5H12.5' stroke='black' strokeLinecap='round' />
       <path
         d='M8.983 7.58704L12.4842 1.52283'
         stroke='black'
-        stroke-linecap='round'
+        strokeLinecap='round'
       />
       <path
         d='M2.517 1.53296L6.01817 7.59716'
         stroke='black'
-        stroke-linecap='round'
+        strokeLinecap='round'
       />
-      <path d='M6.03 11.12L6.03 7.62' stroke='black' stroke-linecap='round' />
+      <path d='M6.03 11.12L6.03 7.62' stroke='black' strokeLinecap='round' />
       <path
         d='M8.94218 13.1214L6.03 11.18'
         stroke='black'
-        stroke-linecap='round'
+        strokeLinecap='round'
       />
       <path
         d='M8.98001 13.12L8.98001 7.62'
         stroke='black'
-        stroke-linecap='round'
+        strokeLinecap='round'
       />
     </svg>
   )
