a8871deb5975eec390995b78f614c6b5d914fc12
Alex <alexandru.pausan@gmail.com>
2023-11-20 14:16:47 +0200

    Refactor left-sidebar components and add iconOverride prop to Section component

--- components/left-sidebar/section.tsx
+++ components/left-sidebar/section.tsx
@@ -6,9 +6,8 @@ import { usePathname } from 'next/navigation'
 import {
   ChevronDownIcon,
   ChevronRightIcon,
-  FileIcon,
   PlusIcon,
-  StarIcon,
+  FileIcon,
 } from '@radix-ui/react-icons'
 
 import {
@@ -19,12 +18,11 @@ import {
 } from '@/components/ui/accordion'
 import { Button } from '@/components/ui/button'
 import { buttonVariants } from '@/components/ui/button'
-import { FilterIcon } from '@/components/ui/icons/custom'
-import path from 'path'
 
 interface SectionProps {
   items: any[]
   title: string
+  iconOverride?: React.FC
 }
 
 const SIDEBAR_BUTTON = cn(buttonVariants({ variant: 'sidebar' }))
@@ -33,7 +31,11 @@ const ACTIVE_SIDEBAR_BUTTON = 'text-primary font-semibold hover:text-primary'
 const TRIGGER_HEADER_STYLE =
   'flex flex-1 justify-between px-4 gap-2 cursor-pointer'
 
-export default function Section({ title, items }: SectionProps) {
+export function Section({
+  title,
+  items,
+  iconOverride: Icon = FileIcon,
+}: SectionProps) {
   const [sectionIsOpen, setSectionIsOpen] = useState(true)
   const pathname = usePathname()
 
@@ -52,12 +54,12 @@ export default function Section({ title, items }: SectionProps) {
             pathname === url && ACTIVE_SIDEBAR_BUTTON,
           )}
         >
-          <FilterIcon />
+          <Icon />
           {name}
         </Link>
       )
     })
-  }, [items, pathname])
+  }, [items, pathname, Icon])
 
   console.log(renderItems)
 
@@ -69,13 +71,19 @@ export default function Section({ title, items }: SectionProps) {
       onValueChange={onToggleSection}
     >
       <AccordionItem value='filters' className='border-none'>
-        <AccordionTrigger asChild className='py-2'>
+        <AccordionTrigger asChild className='py-1'>
           <div className={TRIGGER_HEADER_STYLE}>
-            <Button variant='sidebarSection'>
+            <Button
+              variant='sidebarSection'
+              className='whitespace-nowrap overflow-hidden text-ellipsis'
+            >
               {sectionIsOpen ? <ChevronDownIcon /> : <ChevronRightIcon />}
               {title}
             </Button>
-            <Button variant='sidebar' className='hover:bg-slate-200 -mr-4'>
+            <Button
+              variant='sidebar'
+              className='hover:bg-slate-200 -mr-4 shrink-0'
+            >
               <PlusIcon />
             </Button>
           </div>
--- components/left-sidebar/tag-board.tsx
+++ components/left-sidebar/tag-board.tsx
@@ -1,3 +1,49 @@
-export default function TagBoard() {
-  return <div className='flex flex-col h-full items-center mt-8'>Tag Board</div>
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { StarIcon, TokensIcon } from '@radix-ui/react-icons'
+import { cn } from '@/lib/utils'
+
+import { buttonVariants } from '@/components/ui/button'
+import { Section } from '@/components/left-sidebar/section'
+
+const mockFilters = [{ id: '1', name: 'UX Research', url: '/' }]
+const mockCollections = [{ id: '1', name: 'Group 1', url: '/import' }]
+
+const FAVORITES_URL = '/'
+const SIDEBAR_BUTTON = cn(buttonVariants({ variant: 'sidebar' }))
+const ACTIVE_SIDEBAR_BUTTON = 'text-primary font-semibold hover:text-primary'
+
+export default function ResearchHistory() {
+  const pathname = usePathname()
+  return (
+    <div className='flex flex-col'>
+      <div className='flex flex-col mx-2 items-stretch'>
+        <Link
+          href={FAVORITES_URL}
+          className={cn(
+            SIDEBAR_BUTTON,
+            pathname === FAVORITES_URL && ACTIVE_SIDEBAR_BUTTON,
+          )}
+        >
+          <StarIcon />
+          Favorites
+        </Link>
+
+        <Section
+          title='AI Groups'
+          items={mockFilters}
+          iconOverride={TokensIcon}
+        />
+      </div>
+
+      <div className='flex flex-col px-2 items-stretch'>
+        <Section
+          title='Manual Groups'
+          items={mockCollections}
+          iconOverride={TokensIcon}
+        />
+      </div>
+    </div>
+  )
 }
