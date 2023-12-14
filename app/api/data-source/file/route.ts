85bca57a8a604ea0cbcaf5f77d8b43f6c3ae9dda
Alex <alexandru.pausan@gmail.com>
2023-12-14 20:58:58 +0200

    [api] Refactor file upload and data source creation

--- app/api/data-source/file/route.ts
+++ app/api/data-source/file/route.ts
@@ -1,20 +1,24 @@
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth/next'
-import { DataSourceType, DataSourceStatus } from '@prisma/client'
+import { DataSourceStatus } from '@prisma/client'
 import { Document } from 'langchain/document'
 
 import { authOptions } from '@/lib/authOptions'
 import { uploadToGCS } from '@/lib/cloudStorage'
-import { createDataSource, updateDataSource } from '@/lib/db/dataSource'
-import { getChunksFromDoc } from '@/lib/loaders'
-import { createAndStoreVectors } from '@/lib/qdrant'
+import { updateDataSource } from '@/lib/db/dataSource'
+
+import {
+  processDataSourceCreation,
+  storeVectorsAndUpdateDataSource,
+} from '../utils'
 
 // Upload file from the App
 
 export async function POST(req: Request) {
   const session = (await getServerSession(authOptions)) as ExtendedSession
+  const userId = session?.user?.id
 
-  if (!session?.user?.id) {
+  if (!userId) {
     return new Response('Unauthorized', {
       status: 401,
     })
@@ -29,17 +33,46 @@ export async function POST(req: Request) {
     })
   }
 
-  let collectionId = ''
-  let uploadedFile: File | null = null
+  const { file } = await readFormData(req)
+  if (!file) {
+    return new Response(`Missing file.`, {
+      status: 400,
+    })
+  }
 
-  const userId = session.user?.id
+  const docs = await processDataSourceCreation(file, userId)
+
+  if (docs instanceof Error) {
+    // Handle the error case
+    console.error(docs.message)
+    return new NextResponse(docs.message, {
+      status: 400,
+    })
+  }
+  if (!docs?.length) {
+    return new NextResponse('No docs', {
+      status: 400,
+    })
+  }
+
+  await storeVectorsAndUpdateDataSource(docs)
+
+  // TODO: !!!!
+  // await uploadToGCS({ uploadedFile: file, userId, dataSourceId })
+
+  return NextResponse.json({ docs })
+}
+
+const readFormData = async (req: Request) => {
   const formData = await req.formData()
+  let file: File | null = null
+  let collectionId = ''
 
   for (const value of Array.from(formData.values())) {
     // FormDataEntryValue can either be type `Blob` or `string`.
     // If its type is object then it's a Blob
     if (typeof value == 'object') {
-      uploadedFile = value
+      file = value
     }
     // If its type is string then it's the collectionId
     if (typeof value == 'string') {
@@ -47,59 +80,5 @@ export async function POST(req: Request) {
     }
   }
 
-  if (!uploadedFile) {
-    return new Response(`Missing file.`, {
-      status: 400,
-    })
-  }
-
-  const { name: fileName = '' } = uploadedFile
-
-  // Return nr of chunks & character count
-  const docs = (await getChunksFromDoc({
-    file: uploadedFile,
-    type: DataSourceType.file,
-  })) as Document[] | Error
-
-  if (docs instanceof Error) {
-    // Handle the error case
-    console.error(docs.message)
-    return new NextResponse('Unsupported file type', {
-      status: 400,
-    })
-  }
-
-  const nbChunks = docs.length
-  const textSize = docs.reduce((acc, doc) => acc + doc?.pageContent?.length, 0)
-
-  const dataSourcePayload = {
-    name: fileName,
-    type: DataSourceType.file,
-    title: fileName,
-    nbChunks,
-    textSize,
-    userId,
-  }
-
-  const dataSource = await createDataSource(dataSourcePayload)
-  const dataSourceId = dataSource?.id
-
-  if (!dataSourceId) {
-    return new Response(`Failed to save File. Try again`, {
-      status: 400,
-    })
-  }
-
-  createAndStoreVectors({ docs, metadata: { userId, dataSourceId } })
-
-  try {
-    await uploadToGCS({ uploadedFile, userId, dataSourceId })
-    updateDataSource({ id: dataSourceId, status: DataSourceStatus.synched })
-  } catch (err) {
-    // @TODO: return file upload success, and run the rest of the process in the background
-    updateDataSource({ id: dataSourceId, status: DataSourceStatus.error })
-    console.error(err)
-  }
-
-  return NextResponse.json({ nbChunks, textSize, docs })
+  return { file, collectionId }
 }
