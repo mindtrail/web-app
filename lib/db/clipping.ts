26f7dd0b6776415c63fcc0d3386520382a618abe
Alex <alexandru.pausan@gmail.com>
2024-01-26 11:41:29 +0200

    Updated datasource name to exclude query params or anchors. And added group by capabilities to clipping query.

--- lib/db/clipping.ts
+++ lib/db/clipping.ts
@@ -1,7 +1,25 @@
 import prisma from '@/lib/db/connection'
 
-export async function getClippingList(userId: string) {
-  const clippings = await prisma.clipping.findMany({
+export async function getClippingList(userId: string, groupByDataSource: boolean) {
+  if (groupByDataSource) {
+    // Prisma does not support group by for nested queries.
+    // Used raw sql + build in Postgress json functions to do it
+    return await await prisma.$queryRaw`
+      SELECT
+        d.name as "dataSourceName",
+        json_agg(c) as "clippingList"
+      FROM
+        "Clippings" c
+      INNER JOIN
+        "DataSources" d ON c."dataSourceId" = d.id
+      WHERE
+        c."authorId" = ${userId}
+      GROUP BY
+        d.name
+    `
+  }
+
+  return await prisma.clipping.findMany({
     where: {
       authorId: userId,
     },
@@ -14,7 +32,6 @@ export async function getClippingList(userId: string) {
       },
     },
   })
-  return clippings
 }
 
 export async function createClipping(payload: SavedClipping) {
