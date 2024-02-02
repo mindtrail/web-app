9272c8d94e0400e80047ab243320a1a49b0a173a
Alex <alexandru.pausan@gmail.com>
2024-02-02 10:58:19 +0200

    Add prettier and eslint-config-prettier dependencies, and configure prettier with default settings

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+
+export default {
+  // printWidth: 90,
+  // tabWidth: 2,
+  // semi: false,
+  // singleQuote: true,
+  // jsxSingleQuote: true,
+  // trailingComma: "all",
+  // arrowParens: "always",
+};
