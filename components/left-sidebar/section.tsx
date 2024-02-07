7e3910ae2e2dcc4a7966eb81604be056d7eff646
Alex <alexandru.pausan@gmail.com>
2024-02-07 21:39:40 +0200

    Fixed typi=o

--- components/left-sidebar/section.tsx
+++ components/left-sidebar/section.tsx
@@ -31,9 +31,6 @@ const NESTED_ITEM_STYLE = cn(SIDEBAR_BUTTON, 'pl-8')
 const ACTIVE_SIDEBAR_BUTTON = 'text-primary font-semibold hover:text-primary'
 const TRIGGER_HEADER_STYLE = 'flex flex-1 justify-between px-4 gap-2 cursor-pointer'
 
-<<<<<<< HEAD
-export function Section({ title, items, iconOverride: Icon = FileIcon }: SectionProps) {
-=======
 export function Section({
   title,
   items,
@@ -41,7 +38,6 @@ export function Section({
   // @ts-ignore
   openSecondSidebar,
 }: SectionProps) {
->>>>>>> dev
   const [sectionIsOpen, setSectionIsOpen] = useState(true)
   const pathname = usePathname()
 
