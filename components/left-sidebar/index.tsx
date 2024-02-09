8cf2152ca1c0a251da77713c4f55310d834ccef5
Alex <alexandru.pausan@gmail.com>
2024-02-09 12:40:20 +0200

    Update left sidebar styles and add missing icon in second sidebar

--- components/left-sidebar/index.tsx
+++ components/left-sidebar/index.tsx
@@ -16,36 +16,32 @@ type SidebarNavProps = {
 
 const BRAND_NAME = 'Mind Trail'
 
-export function LeftSidebar({ className, user }: SidebarNavProps) {
+export function LeftSidebar({ user }: SidebarNavProps) {
   return (
-    <div className='min-h-screen flex relative'>
+    <div className='min-h-screen flex flex-col'>
       {/* Container for both sidebars and main content */}
-      <div className={`flex ${className}`}>
-        {/* First sidebar */}
-        <nav className='flex flex-col justify-between w-[256px] border-r flex-shrink-0 overflow-hidden transition-all duration-300 ease-in-out h-screen'>
-          <div>
-            <div className='h-14 border-b flex items-center justify-center'>
-              <Link
-                href='/'
-                className='flex gap-4 w-full px-6 py-2 self-center items-center'
-              >
-                <Image width={30} height={30} src='/icon-2.png' alt='Mind Trail' />
-                {BRAND_NAME}
-              </Link>
-            </div>
+      {/* First sidebar */}
+      <nav
+        className='flex flex-col flex-1 justify-between w-[256px] flex-shrink-0
+            border-r overflow-hidden transition-all duration-300 ease-in-out h-screen'
+      >
+        <div className='h-14 border-b flex items-center justify-center'>
+          <Link href='/' className='flex gap-4 w-full px-4 py-2 self-center items-center'>
+            <Image width={30} height={30} src='/icon-2.png' alt='Mind Trail' />
+            {BRAND_NAME}
+          </Link>
+        </div>
 
-            <TopSection />
+        <TopSection />
 
-            <Separator />
+        <Separator />
 
-            <Folders />
-          </div>
+        <Folders />
 
-          <div className='p-4 border-t border-gray-200'>
-            <LeftSidebarFooter user={user} />
-          </div>
-        </nav>
-      </div>
+        <div className='p-4 border-t border-gray-200'>
+          <LeftSidebarFooter user={user} />
+        </div>
+      </nav>
     </div>
   )
 }
