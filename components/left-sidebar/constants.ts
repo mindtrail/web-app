4a47e1e3d8af6c4befb07c8a574c070dc0013142
Alex <alexandru.pausan@gmail.com>
2024-02-09 20:52:46 +0200

    Refactor left-sidebar component to use useEffect and useState hooks

--- components/left-sidebar/constants.ts
+++ components/left-sidebar/constants.ts
@@ -5,6 +5,8 @@ import {
   IconTag,
 } from '@/components/ui/icons/next-icons'
 
+export const APP_NAME = 'Mind Trail'
+
 export const TOP_SIDEBAR_ITEMS = [
   { name: 'All Items', url: '/all-items', icon: IconAllData },
   { name: 'Highlights', url: '/highlights', icon: IconHighlight },
