286edcde742ccce22813deb52bdbf10a72f49000
Alex <alexandru.pausan@gmail.com>
2024-02-09 16:52:30 +0200

    Refactoring

--- components/left-sidebar/top-section.tsx
+++ components/left-sidebar/top-section.tsx
@@ -2,12 +2,11 @@ import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
 import { cn } from '@/lib/utils'
-
 import { buttonVariants } from '@/components/ui/button'
 import { IconAllData, IconHighlight } from '@/components/ui/icons/next-icons'
 
-const SIDEBAR_BUTTON = cn(buttonVariants({ variant: 'sidebar' }))
-const ACTIVE_SIDEBAR_BUTTON = 'text-primary font-semibold'
+const SIDEBAR_BTN = cn(buttonVariants({ variant: 'sidebar' }))
+const ACTIVE_BTN = cn(buttonVariants({ variant: 'sidebarActive' }))
 
 const topSectionItems = [
   { name: 'All Items', url: '/all-items', icon: IconAllData },
@@ -18,12 +17,12 @@ export function TopSection() {
   const pathname = usePathname()
 
   return (
-    <div className='flex flex-col py-2 px-2 gap-1 '>
+    <div className='flex flex-col py-2 gap-1 '>
       {topSectionItems.map(({ name, url, icon: Icon }) => (
         <Link
           key={url}
           href={url}
-          className={cn(SIDEBAR_BUTTON, pathname === url && ACTIVE_SIDEBAR_BUTTON)}
+          className={cn(SIDEBAR_BTN, pathname === url && ACTIVE_BTN)}
         >
           <Icon />
           {name}
