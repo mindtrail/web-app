063960818cca9e0881f5ba1100f4e364eac0a6e8
Alex <alexandru.pausan@gmail.com>
2024-02-29 17:40:25 +0200

    Increase MAX_COL_SIZE to 800 and style the preview item container

--- components/history/preview.tsx
+++ components/history/preview.tsx
@@ -20,35 +20,24 @@ export const PreviewItem = ({ previewItem, setPreviewItem }: PreviewProps) => {
     return () => window.removeEventListener('message', onMessage)
   }, [])
 
-  const renderIframe = () => {
-    try {
-      return (
-        <iframe
-          className='flex-1 w-full rounded-md'
-          loading='lazy'
-          allow='clipboard-write; encrypted-media; fullscreen'
-          referrerPolicy='no-referrer-when-downgrade'
-          src={previewItem?.name}
-          title='YouTube video player'
-          onError={(msg) => console.log('iframe NOT loaded', msg)}
-        />
-      )
-    } catch (err) {
-      console.log(err)
-      return <div>Error</div>
-    }
-  }
-
   return (
-    <div className='flex flex-col h-full p-4'>
-      <div className='flex justify-between items-center mb-2 gap-4'>
+    <div className='flex flex-col h-full bg-muted'>
+      <div className='flex justify-between items-center p-2 gap-4'>
         <Button onClick={() => setPreviewItem(null)} variant='ghost'>
           <Cross1Icon className='w-4 h-4' />
         </Button>
         {/* {previewItem?.title} */}
       </div>
 
-      {renderIframe()}
+      <iframe
+        className='flex-1 w-full border'
+        loading='lazy'
+        allow='clipboard-write; encrypted-media; fullscreen'
+        referrerPolicy='no-referrer-when-downgrade'
+        src={previewItem?.name}
+        title='YouTube video player'
+        onError={(msg) => console.log('iframe NOT loaded', msg)}
+      />
     </div>
   )
 }
