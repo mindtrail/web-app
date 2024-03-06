d91f0623c3ab36f312307363e47bfd45747b29e3
Alex <alexandru.pausan@gmail.com>
2024-03-06 18:29:11 +0200

    Refactor PDFViewer component and add check for file and pdfWorkerUrl before rendering

--- components/history/pdf-viewer.tsx
+++ components/history/pdf-viewer.tsx
@@ -4,14 +4,22 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css'
 import { Viewer, Worker } from '@react-pdf-viewer/core'
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
 
+import { usePDFWorker } from '@/lib/hooks/use-PDF-worker'
+
 export function PDFViewer({ file }: { file: string }) {
+  const workerUrl = 'https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js'
+  const pdfWorkerUrl = usePDFWorker(workerUrl)
   const defaultLayoutPluginInstance = defaultLayoutPlugin()
 
+  if (!file || !pdfWorkerUrl) {
+    return null
+  }
+
   return (
-    <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js'>
-      <div style={{ height: '750px' }}>
-        <Viewer fileUrl={file} plugins={[defaultLayoutPluginInstance]} />
-      </div>
+    <Worker workerUrl={pdfWorkerUrl}>
+      {/* <div> */}
+      <Viewer fileUrl={file} plugins={[defaultLayoutPluginInstance]} />
+      {/* </div> */}
     </Worker>
   )
 }
