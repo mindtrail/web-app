2acf1d092c83080fce925703b4926e2b34254921
Alex <alexandru.pausan@gmail.com>
2024-03-19 17:15:58 +0200

    Remove async from LeftSidebar function and optimize root layout condition rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,4 @@
-import { redirect } from 'next/navigation'
-import { getServerSession } from 'next-auth/next'
-
-import { authOptions } from '@/lib/authOptions'
-
 export default async function Home() {
-  const session = await getServerSession(authOptions)
-
-  if (!session) {
-    redirect(`/api/auth/signin?callbackUrl=/protected`)
-  }
-
   return (
     <div className='flex flex-col flex-1 w-full items-center bg-muted/50'>
       <div className='flex flex-col flex-1 w-full max-w-6xl'>
