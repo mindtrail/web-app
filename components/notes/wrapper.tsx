d64fe7ffd573f79a07ce883d60d9496dd9bb71da
Alex <alexandru.pausan@gmail.com>
2024-03-21 16:35:05 +0200

    Update checklist tool with inline toolbar options and clean up unused code and comments

--- components/notes/wrapper.tsx
+++ components/notes/wrapper.tsx
@@ -1,41 +1,38 @@
 import { useEffect, useRef, useState, memo } from 'react'
-import EditorJS, { EditorConfig, OutputData } from '@editorjs/editorjs' // @ts-ignore
-import Header from '@editorjs/header' // @ts-ignore
-import Link from '@editorjs/link' // @ts-ignore
-import List from '@editorjs/list' // @ts-ignore
+import EditorJS from '@editorjs/editorjs' // @ts-ignore
 
 import { EDITORJS_CONFIG } from './utils'
 
 function EditorWrapper() {
   const editorRef = useRef<EditorJS | null>(null)
-  const [editorInstance, setEditor] = useState(null)
+  const [editorInstance, setEditor] = useState<EditorJS | null>(null)
 
+  // initialize
   useEffect(() => {
-    if (!editorRef?.current) {
-      initEditor()
-    }
-
-    function initEditor() {
-      const editor = new EditorJS({
-        ...EDITORJS_CONFIG,
-        holder: 'editor-wrapper',
-        onReady: () => {
-          console.log(editor)
-        },
-        onChange: async () => {
-          let content = await editor.saver.save()
-          // console.log(content)
-        },
-      })
-      editorRef.current = editor
-    }
+    const editor = new EditorJS({ ...EDITORJS_CONFIG, holder: 'editor-wrapper' })
+    setEditor(editor)
 
     return () => {
-      if (editorRef?.current && editorRef?.current?.destroy) {
-        editorRef.current.destroy()
-      }
+      editor.isReady
+        .then(() => {
+          editor.destroy()
+          setEditor(null)
+        })
+        .catch((e) => console.error('ERROR editor cleanup', e))
     }
   }, [])
+
+  // set reference
+  useEffect(() => {
+    console.log(editorInstance, editorRef)
+    if (!editorInstance) {
+      return
+    }
+    // Send instance to the parent
+    if (editorRef) {
+      editorRef.current = editorInstance
+    }
+  }, [editorInstance, editorRef])
 
   return <div id='editor-wrapper' className='h-full' />
 }
