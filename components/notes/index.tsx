477a261fe12207d9bd7648ffc1e1aea69dcd6b8c
Alex <alexandru.pausan@gmail.com>
2024-03-26 20:59:50 +0200

    Fix crash when rendering server side for notes editor by updating editor wrapper

--- components/notes/index.tsx
+++ components/notes/index.tsx
@@ -9,13 +9,12 @@ import { Typography } from '@/components/typography'
 // import { useGlobalState, useGlobalStateActions } from '@/context/global-state'
 // import { Separator } from '@/components/ui/separator'
 
+// This will crash if rendered server side and this solves it
 const Editor = dynamic(() => import('./wrapper'), { ssr: false })
 
 export function NotesComponent() {
   const [editor, setEditor] = useState(null)
-
   // const [state] = useGlobalState()
-
   // const { activeNestedSidebar, nestedItemsByCategory } = state
   // const { setActiveNestedSidebar, setNestedItemsByCategory } = useGlobalStateActions()
 
--- components/notes/utils.ts
+++ components/notes/utils.ts
@@ -1,8 +1,8 @@
-import { BlockNoteEditorOptions, PartialBlock } from '@blocknote/core' // @ts-ignore
+import { PartialBlock } from '@blocknote/core' // @ts-ignore
 import AIText from './ai-autocomplete'
 // import AIText from '@alkhipce/editorjs-aitext'
 
-const mockData: PartialBlock[] = [
+const initialContent: PartialBlock[] = [
   {
     type: 'paragraph',
     content: 'Welcome to this demo!',
@@ -21,6 +21,28 @@ const mockData: PartialBlock[] = [
 ]
 
 const PLACEHOLDER_MSG = "Write, press 'space' for AI or '/' for more"
-const editorOptions: BlockNoteEditorOptions = {
-  
+export const EDITOR_OPTIONS = {
+  initialContent,
+  uploadFile: async (file: File): Promise<string> => {
+    console.log('file', file)
+    const result = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const res = {
+          success: true,
+          file: {
+            url: URL.createObjectURL(file),
+            name: file.name,
+            size: file.size,
+            type: file.type,
+          },
+        }
+
+        resolve(res)
+      }, 1000)
+
+      console.log(result)
+    })
+
+    return 'true'
+  },
 }
