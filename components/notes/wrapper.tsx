37c3f1659e92af7f1bbf22a1042a09a56c41cd3c
Alex <alexandru.pausan@gmail.com>
2024-03-26 20:59:50 +0200

    Fix crash when rendering server side for notes editor by updating editor wrapper

--- components/notes/wrapper.tsx
+++ components/notes/wrapper.tsx
@@ -1,11 +1,26 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { useTheme } from 'next-themes'
 
 import { BlockNoteView, useCreateBlockNote } from '@blocknote/react'
 import '@blocknote/core/fonts/inter.css'
 import '@blocknote/react/style.css'
 
-export default function EditorWrapper() {
-  const editor = useCreateBlockNote()
+import { EDITOR_OPTIONS } from './utils'
 
-  return <BlockNoteView editor={editor} theme={'light'} />
+export default function EditorWrapper() {
+  const editor = useCreateBlockNote(EDITOR_OPTIONS)
+  const { theme } = useTheme()
+
+  const editorTheme = useMemo(() => {
+    switch (theme) {
+      case 'dark':
+        return 'dark'
+      case 'light':
+        return 'light'
+      default: // for system theme, we don't set anything
+        return
+    }
+  }, [theme])
+
+  return <BlockNoteView editor={editor} theme={editorTheme} />
 }
