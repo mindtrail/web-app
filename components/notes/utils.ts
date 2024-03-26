93de75a1465afe66864eb01623cd5b70d79a7423
Alex <alexandru.pausan@gmail.com>
2024-03-26 22:15:23 +0200

    Add mention inline content and updated block types and styles

--- components/notes/utils.ts
+++ components/notes/utils.ts
@@ -9,11 +9,90 @@ const initialContent: PartialBlock[] = [
   },
   {
     type: 'paragraph',
-    content: '<- Click the Drag Handle to see the new item',
+  },
+  {
+    type: 'paragraph',
+    content: [
+      {
+        type: 'text',
+        text: 'Blocks:',
+        styles: { bold: true },
+      },
+    ],
+  },
+  {
+    type: 'paragraph',
+    content: 'Paragraph',
+  },
+  {
+    type: 'heading',
+    content: 'Heading',
   },
   {
     type: 'bulletListItem',
-    content: "Try resetting this block's type using the new Drag Handle Menu item",
+    content: 'Bullet List Item',
+  },
+  {
+    type: 'numberedListItem',
+    content: 'Numbered List Item',
+  },
+  {
+    type: 'image',
+  },
+  {
+    type: 'table',
+    content: {
+      type: 'tableContent',
+      rows: [
+        {
+          cells: ['Table Cell', 'Table Cell', 'Table Cell'],
+        },
+        {
+          cells: ['Table Cell', 'Table Cell', 'Table Cell'],
+        },
+        {
+          cells: ['Table Cell', 'Table Cell', 'Table Cell'],
+        },
+      ],
+    },
+  },
+  {
+    type: 'paragraph',
+  },
+  {
+    type: 'paragraph',
+    content: [
+      {
+        type: 'text',
+        text: 'Inline Content:',
+        styles: { bold: true },
+      },
+    ],
+  },
+  {
+    type: 'paragraph',
+    content: [
+      {
+        type: 'text',
+        text: 'Styled Text',
+        styles: {
+          bold: true,
+          italic: true,
+          textColor: 'red',
+          backgroundColor: 'blue',
+        },
+      },
+      {
+        type: 'text',
+        text: ' ',
+        styles: {},
+      },
+      {
+        type: 'link',
+        content: 'Link',
+        href: 'https://www.blocknotejs.org',
+      },
+    ],
   },
   {
     type: 'paragraph',
