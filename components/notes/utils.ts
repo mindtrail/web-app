05c3aceedf996156c24ba2816e895cca43e52103
Alex <alexandru.pausan@gmail.com>
2024-03-26 20:22:42 +0200

    Update notes component UI layout and editor options

--- components/notes/utils.ts
+++ components/notes/utils.ts
@@ -1,126 +1,26 @@
-// @ts-nocheck
-
-import { EditorConfig, OutputData } from '@editorjs/editorjs' // @ts-ignore
-import Header from '@editorjs/header'
-import NestedList from '@editorjs/nested-list'
-import Paragraph from '@editorjs/paragraph'
-import LinkTool from '@editorjs/link'
-import LinkAutocomplete from '@editorjs/link-autocomplete'
-import Delimiter from '@editorjs/delimiter'
-import SimpleImage from '@editorjs/simple-image'
-import Embed from '@editorjs/embed'
-import EJLaTeX from 'editorjs-latex'
-import IndentTune from 'editorjs-indent-tune'
-import TextAlignment from 'editorjs-text-alignment-blocktune'
-import TextVariantTune from '@editorjs/text-variant-tune'
-import Strikethrough from '@sotaproject/strikethrough'
-import Underline from '@editorjs/underline'
+import { BlockNoteEditorOptions, PartialBlock } from '@blocknote/core' // @ts-ignore
 import AIText from './ai-autocomplete'
 // import AIText from '@alkhipce/editorjs-aitext'
 
-const PLACEHOLDER_MSG = "Write, press 'space' for AI or '/' for more"
-const mockData = [
+const mockData: PartialBlock[] = [
   {
-    type: 'header',
-    data: {
-      text: 'A heading',
-      level: 2,
-    },
+    type: 'paragraph',
+    content: 'Welcome to this demo!',
   },
   {
     type: 'paragraph',
-    data: {
-      text: 'Paragraph with some <b>bold</b>&nbsp;text. And <a href="https://google.com">some</a> <i>italic</i>&nbsp;text.',
-    },
+    content: '<- Click the Drag Handle to see the new item',
+  },
+  {
+    type: 'bulletListItem',
+    content: "Try resetting this block's type using the new Drag Handle Menu item",
   },
   {
     type: 'paragraph',
-    data: {
-      text: 'A lovely editor I must say.',
-    },
-  },
-  {
-    type: 'paragraph',
-    data: {
-      text: 'Save new data.',
-    },
   },
 ]
 
-const DEFAULT_INITIAL_DATA: OutputData = {
-  time: new Date().getTime(),
-  blocks: mockData,
-}
-
-const EDITORJS_TOOLS = {
-  paragraph: {
-    class: Paragraph,
-    inlineToolbar: true,
-    config: {
-      placeholder: PLACEHOLDER_MSG,
-    },
-    tunes: ['textAlign', 'textVariant', 'indentTune'],
-  },
-  header: {
-    class: Header,
-    inlineToolbar: true,
-    config: {
-      defaultLevel: 2,
-      placeholder: 'Heading...',
-    },
-    tunes: ['textAlign', 'textVariant', 'indentTune'],
-  },
-  list: {
-    class: NestedList,
-    inlineToolbar: true,
-    config: {
-      defaultStyle: 'unordered',
-    },
-    tunes: [],
-  },
-  embed: {
-    class: Embed,
-  },
-  linkTool: {
-    class: LinkTool,
-    config: {
-      endpoint: '/api/editor/link-embed',
-    },
-  },
-  underline: Underline,
-  strikethrough: Strikethrough,
-  link: {
-    class: LinkAutocomplete,
-    config: {
-      endpoint: '/api/editor/link-autocomplete',
-      queryParam: 'url',
-    },
-  },
-  simpleImage: {
-    class: SimpleImage,
-    tunes: ['textAlign', 'indentTune'],
-  },
-
-  delimiter: Delimiter,
-  Math: {
-    class: EJLaTeX,
-    shortcut: 'CMD+SHIFT+M',
-    tunes: ['textAlign', 'textVariant', 'indentTune'],
-  },
-
-  aiText: {
-    class: AIText as unknown as ToolConstructable,
-    config: {
-      openaiKey: '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',
-    },
-  },
-  textVariant: TextVariantTune,
-  indentTune: IndentTune,
-  textAlign: TextAlignment,
-}
-
-export const EDITORJS_CONFIG: EditorConfig = {
-  autofocus: true,
-  data: DEFAULT_INITIAL_DATA,
-  tools: EDITORJS_TOOLS,
+const PLACEHOLDER_MSG = "Write, press 'space' for AI or '/' for more"
+const editorOptions: BlockNoteEditorOptions = {
+  
 }
--- components/notes/wrapper.tsx
+++ components/notes/wrapper.tsx
@@ -1,56 +1,11 @@
 import { useEffect, useRef, useState } from 'react'
-import EditorJS, { EditorConfig } from '@editorjs/editorjs'
-// @ts-ignore
-import DragDrop from 'editorjs-drag-drop'
-import { EDITORJS_CONFIG } from './utils'
 
-let instanceNr = 0
+import { BlockNoteView, useCreateBlockNote } from '@blocknote/react'
+import '@blocknote/core/fonts/inter.css'
+import '@blocknote/react/style.css'
 
-function EditorWrapper() {
-  const editorRef = useRef<EditorJS | null>(null)
-  const [val, setValue] = useState('')
+export default function EditorWrapper() {
+  const editor = useCreateBlockNote()
 
-  console.log(1232114)
-  useEffect(() => {
-    // Because of react strict mode, the component loads twice, and destroys the editor
-    // on first cleanup. So we skip the initialization in this case.
-    if (process.env.NODE_ENV === 'development' && instanceNr === 0) {
-      instanceNr++
-      return
-    }
-
-    const EditorOpts: EditorConfig = {
-      ...EDITORJS_CONFIG,
-      holder: 'editor-wrapper',
-      onReady: async () => {
-        // new DragDrop(editor)
-        // setEditorInstance(editor)
-      },
-      onChange: async (api) => {
-        // Handle change if needed
-        console.log(await api.saver.save())
-      },
-    }
-
-    editorRef.current = new EditorJS(EditorOpts)
-
-    return () => {
-      if (editorRef.current) {
-        try {
-          editorRef.current?.destroy()
-        } catch (e) {
-          console.error(e)
-        } finally {
-          console.log(333)
-          // reset the count and ref
-          instanceNr = 0
-          editorRef.current = null
-        }
-      }
-    }
-  }, [])
-
-  return <div id='editor-wrapper' className='h-full' />
+  return <BlockNoteView editor={editor} theme={'light'} />
 }
-
-export default EditorWrapper
