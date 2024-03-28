8d675f829767941a3f47ec5edc3b8eb530113fcf
Alex <alexandru.pausan@gmail.com>
2024-03-28 18:56:50 +0200

    Add dynamic "Turn into" menu item based on block type in CustomSideMenu component

--- components/notes/custom/side-menu.tsx
+++ components/notes/custom/side-menu.tsx
@@ -1,18 +1,19 @@
 import { BlockNoteEditor } from '@blocknote/core'
 import {
   BlockColorsItem,
-  ColorStyleButton,
   DragHandleMenu,
-  DragHandleMenuItem,
   RemoveBlockItem,
   SideMenu,
   SideMenuController,
+  DragHandleMenuItem,
 } from '@blocknote/react'
 
 import { TrashIcon, SymbolIcon } from '@radix-ui/react-icons'
 
 import { ColorPaletteIcon } from '@/components/ui/icons/next-icons'
 import { Separator } from '@/components/ui/separator'
+
+import { TurnIntoMenuItem } from './turn-into'
 
 interface CustomSideMenuProps {
   editor: BlockNoteEditor<any, any, any>
@@ -26,23 +27,21 @@ export function CustomSideMenu({ editor }: CustomSideMenuProps) {
           dragHandleMenu={(props) => (
             <DragHandleMenu {...props} data-theming-mindtrail-demo='true'>
               <BlockColorsItem {...props}>
-                <div className='flex gap-1'>
+                <div className='flex gap-1 '>
                   <ColorPaletteIcon className='w-4 h-4' />
                   Colors
                 </div>
               </BlockColorsItem>
-              {/* Item which resets the hovered block's type. */}
-              <DragHandleMenuItem
-                onClick={() => {
-                  editor.updateBlock(props.block, { type: 'paragraph' })
-                }}
-              >
-                <div className='flex gap-1'>
-                  <SymbolIcon className='w-4 h-4' />
-                  Turn into
-                </div>
-              </DragHandleMenuItem>
-              {/* <BlockTypeSelect>Change Type</BlockTypeSelect> */}
+
+              {props.block.type !== 'image' && (
+                <TurnIntoMenuItem {...props}>
+                  <div className='flex gap-1'>
+                    <SymbolIcon className='w-4 h-4' />
+                    Turn into
+                  </div>
+                </TurnIntoMenuItem>
+              )}
+
               <Separator className='my-2' />
               <RemoveBlockItem {...props}>
                 <div className='flex gap-1'>
