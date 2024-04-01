22c5dda6f52ee0b670708073635fc296566d0325
Alex <alexandru.pausan@gmail.com>
2024-04-01 22:45:09 +0300

    Add InlineToolbar component and enhance AI functionalities in EditorWrapper

--- components/notes/inlineToolbar/index.tsx
+++ components/notes/inlineToolbar/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState, ReactNode } from 'react'
+import { useEffect, useState, useRef, ReactNode } from 'react'
 import { EditorBubble, useEditor } from 'novel'
 import { removeAIHighlight } from 'novel/extensions'
 
@@ -8,68 +8,71 @@ import { Button } from '@/components/ui/button'
 import { MagicIcon } from '@/components/ui/icons/custom'
 import { Separator } from '@/components/ui/separator'
 
-import { AISelector } from './generative/ai-selector'
+import { AISelector } from '../generative/ai-selector'
 import { NodeSelector } from './selectors/node-selector'
 import { LinkSelector } from './selectors/link-selector'
 import { ColorSelector } from './selectors/color-selector'
 import { TextButtons } from './selectors/text-buttons'
 
-interface GenerativeMenuSwitchProps {
+interface InlineToolbarProps {
   children?: ReactNode
-  open: boolean
-  onOpenChange: (open: boolean) => void
 }
-export const GenerativeMenuSwitch = (props: GenerativeMenuSwitchProps) => {
-  const { children, open, onOpenChange } = props
+export const InlineToolbar = (props: InlineToolbarProps) => {
+  const { children } = props
   const { editor } = useEditor()
+  const toolbarRef = useRef<HTMLDivElement>(null)
 
   const [openNode, setOpenNode] = useState(false)
   const [openColor, setOpenColor] = useState(false)
   const [openLink, setOpenLink] = useState(false)
+  const [openAIEditor, setOpenAIEditor] = useState(false)
 
   useEffect(() => {
     if (!open && editor) {
       removeAIHighlight(editor)
     }
-  }, [open, editor])
+  }, [openAIEditor, editor])
 
   if (!editor) return null
+  console.log(111)
 
   return (
-    <EditorBubble
-      tippyOptions={{
-        placement: open ? 'bottom-start' : 'top',
-        onHidden: () => {
-          onOpenChange(false)
-          editor.chain().unsetHighlight().run()
-        },
-      }}
-      className='flex w-fit max-w-[90vw] overflow-hidden rounded-md border border-muted bg-background shadow-xl'
-    >
-      {open && <AISelector open={open} onOpenChange={onOpenChange} />}
-      {open && (
-        <>
-          <Button
-            className='gap-1 rounded-none text-purple-500'
-            variant='ghost'
-            onClick={() => onOpenChange(true)}
-            size='sm'
-          >
-            <MagicIcon className='h-5 w-5' />
-            Ask AI
-          </Button>
-          <Separator orientation='vertical' />
-          <NodeSelector open={openNode} onOpenChange={setOpenNode} />
-          <Separator orientation='vertical' />
+    <div ref={toolbarRef} className='1234'>
+      <EditorBubble
+        tippyOptions={{
+          appendTo: document.body,
+          placement: 'top', // openAIEditor ? 'bottom-start' :
+          onHidden: () => {
+            setOpenAIEditor(false)
+            editor.chain().unsetHighlight().run()
+          },
+        }}
+        className='flex w-fit max-w-[90vw] rounded-md border border-muted bg-background shadow-xl'
+      >
+        <Button
+          className='gap-1 rounded-none text-purple-500'
+          variant='ghost'
+          onClick={() => setOpenAIEditor(true)}
+          size='sm'
+        >
+          <MagicIcon className='h-5 w-5' />
+          Ask AI
+        </Button>
+        <Separator orientation='vertical' />
+        <NodeSelector open={openNode} onOpenChange={setOpenNode} />
+        <Separator orientation='vertical' />
 
-          <LinkSelector open={openLink} onOpenChange={setOpenLink} />
-          <Separator orientation='vertical' />
-          <TextButtons />
-          <Separator orientation='vertical' />
-          <ColorSelector open={openColor} onOpenChange={setOpenColor} />
-          {children}
-        </>
-      )}
-    </EditorBubble>
+        <LinkSelector open={openLink} onOpenChange={setOpenLink} />
+        <Separator orientation='vertical' />
+        <TextButtons />
+        <Separator orientation='vertical' />
+        <ColorSelector open={openColor} onOpenChange={setOpenColor} />
+        {children}
+
+        {openAIEditor && (
+          <AISelector open={openAIEditor} onOpenChange={setOpenAIEditor} />
+        )}
+      </EditorBubble>
+    </div>
   )
 }
