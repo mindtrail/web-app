4b0183fbddf815812eecce502d7afe9054ba62f6
Alex <alexandru.pausan@gmail.com>
2024-04-01 20:58:10 +0300

    Add OpenAI API integration for chat completion and rate limiting

--- components/notes/index.tsx
+++ components/notes/index.tsx
@@ -1,17 +1,9 @@
 'use client'
-import { useEffect, useRef, useState, memo } from 'react'
-
-import dynamic from 'next/dynamic'
-
 import { Separator } from '@/components/ui/separator'
 import { Typography } from '@/components/typography'
-
-// This will crash if rendered server side and this solves it
-const Editor = dynamic(() => import('./editor-wrapper'), { ssr: false })
+import Editor from './editor-wrapper'
 
 export function NotesComponent() {
-  const [editor, setEditor] = useState(null)
-
   return (
     <div id='notes' className='flex flex-col flex-1 h-screen overflow-hidden'>
       <div className='py-8 px-16'>
