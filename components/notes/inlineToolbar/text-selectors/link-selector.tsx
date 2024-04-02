f3ad0be7d46d12469212384268966a5f3c7bcb64
Alex <alexandru.pausan@gmail.com>
2024-04-02 20:25:04 +0300

    Refactor LinkSelector to simplify and improve code readability

--- components/notes/inlineToolbar/text-selectors/link-selector.tsx
+++ components/notes/inlineToolbar/text-selectors/link-selector.tsx
@@ -8,7 +8,7 @@ import { Input } from '@/components/ui/input'
 
 import { PopoverSelector } from '../popover-selector'
 
-export const LinkSelector = ({ open, onOpenChange }: LinkSelectorProps) => {
+export const LinkSelector = () => {
   const { editor } = useEditor()
   const [url, setUrl] = useState('')
 
@@ -28,36 +28,54 @@ export const LinkSelector = ({ open, onOpenChange }: LinkSelectorProps) => {
     [editor],
   )
 
-  const LinkSelectorContent = useCallback(() => {
-    const handleUrlChange = (e: any) => {
-      setUrl(e.target.value)
-    }
-
-    const handleSubmit = (e: any) => {
-      e.preventDefault()
-      if (!isValidUrl(url)) {
-        alert('Please enter a valid URL')
-        return
+  const LinkSelectorContent = useCallback(
+    (closeModal: () => void) => {
+      const handleUrlChange = (e: any) => {
+        setUrl(e.target.value)
       }
-      editor?.chain().focus().extendMarkRange('link').setLink({ href: url }).run()
-      onOpenChange(false)
-    }
 
-    return (
-      <form onSubmit={handleSubmit} className='p-4'>
-        <Input
-          autoFocus
-          placeholder='Paste or type a link...'
-          value={url}
-          onChange={handleUrlChange}
-          className='mb-2'
-        />
-        <Button type='submit' variant='primary' className='w-full'>
-          Apply
-        </Button>
-      </form>
-    )
-  }, [url])
+      const handleSubmit = (e: any) => {
+        e.preventDefault()
+        if (!isValidUrl(url)) {
+          alert('Please enter a valid URL')
+          return
+        }
+        editor?.chain().focus().extendMarkRange('link').setLink({ href: url }).run()
+        closeModal()
+      }
+
+      return (
+        <form onSubmit={handleSubmit} className='flex p-1 gap-1 items-center'>
+          <Input
+            autoFocus
+            placeholder='Paste or type a link...'
+            value={url}
+            onChange={handleUrlChange}
+            className='border-none text-sm shadow-none'
+          />
+          {editor?.getAttributes('link').href ? (
+            <Button
+              size='icon'
+              variant='outline'
+              type='button'
+              className='flex h-8 items-center rounded-sm text-red-600 transition-all hover:bg-red-100 dark:hover:bg-red-800'
+              onClick={() => {
+                editor?.chain().focus().unsetLink().run()
+                closeModal()
+              }}
+            >
+              <Trash className='h-4 w-4' />
+            </Button>
+          ) : (
+            <Button size='icon' className='h-8'>
+              <Check className='h-4 w-4' />
+            </Button>
+          )}
+        </form>
+      )
+    },
+    [url, editor],
+  )
 
   if (!editor) return null
 
@@ -87,61 +105,4 @@ export function getUrlFromString(str: string) {
   } catch (e) {
     return null
   }
-}
-interface LinkSelectorProps {
-  open: boolean
-  onOpenChange: (open: boolean) => void
-}
-
-export const LinkSelector2 = ({ open, onOpenChange }: LinkSelectorProps) => {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const { editor } = useEditor()
-
-  // Autofocus on input by default
-  useEffect(() => {
-    inputRef.current && inputRef.current?.focus()
-  })
-  if (!editor) return null
-
-  return (
-    <Popover modal={true} open={open} onOpenChange={onOpenChange}>
-      <PopoverContent align='start' className='w-60 p-0' sideOffset={10}>
-        <form
-          onSubmit={(e) => {
-            const target = e.currentTarget as HTMLFormElement
-            e.preventDefault()
-            const input = target[0] as HTMLInputElement
-            const url = getUrlFromString(input.value)
-            url && editor.chain().focus().setLink({ href: url }).run()
-          }}
-          className='flex  p-1 '
-        >
-          <input
-            ref={inputRef}
-            type='text'
-            placeholder='Paste a link'
-            className='flex-1 bg-background p-1 text-sm outline-none'
-            defaultValue={editor.getAttributes('link').href || ''}
-          />
-          {editor.getAttributes('link').href ? (
-            <Button
-              size='icon'
-              variant='outline'
-              type='button'
-              className='flex h-8 items-center rounded-sm p-1 text-red-600 transition-all hover:bg-red-100 dark:hover:bg-red-800'
-              onClick={() => {
-                editor.chain().focus().unsetLink().run()
-              }}
-            >
-              <Trash className='h-4 w-4' />
-            </Button>
-          ) : (
-            <Button size='icon' className='h-8'>
-              <Check className='h-4 w-4' />
-            </Button>
-          )}
-        </form>
-      </PopoverContent>
-    </Popover>
-  )
 }
