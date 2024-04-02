703326092fb731d6354834ebca448b642afe3270
Alex <alexandru.pausan@gmail.com>
2024-04-02 19:23:04 +0300

    Add PopoverSelector component for NodeSelector in inlineToolbar

--- components/notes/inlineToolbar/text-selectors/node-selector.tsx
+++ components/notes/inlineToolbar/text-selectors/node-selector.tsx
@@ -15,6 +15,7 @@ import { EditorBubbleItem, EditorInstance, useEditor } from 'novel'
 
 import { Popover, PopoverContent, PopoverTrigger } from '@/components//ui/popover'
 import { Button } from '@/components//ui/button'
+import { PopoverSelector } from '../popover-selector'
 
 export type SelectorItem = {
   name: string
@@ -99,37 +100,39 @@ export const NodeSelector = ({ open, onOpenChange }: NodeSelectorProps) => {
     name: 'Multiple',
   }
 
-  return (
-    <Popover modal={true} open={open} onOpenChange={onOpenChange}>
-      <PopoverTrigger
-        asChild
-        className='gap-2 rounded-none border-none hover:bg-accent focus:ring-0'
+  const NodeSelectorContent = () =>
+    items.map((item, index) => (
+      <EditorBubbleItem
+        key={index}
+        onSelect={(editor) => {
+          item.command(editor)
+          onOpenChange(false)
+        }}
+        className='flex cursor-pointer items-center justify-between rounded-sm px-2 py-1 text-sm hover:bg-accent'
       >
-        <Button size='sm' variant='ghost' className='gap-2'>
-          <span className='whitespace-nowrap text-sm'>{activeItem.name}</span>
-          <ChevronDown className='h-4 w-4' />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent sideOffset={5} align='start' className='w-48 p-1'>
-        {items.map((item, index) => (
-          <EditorBubbleItem
-            key={index}
-            onSelect={(editor) => {
-              item.command(editor)
-              onOpenChange(false)
-            }}
-            className='flex cursor-pointer items-center justify-between rounded-sm px-2 py-1 text-sm hover:bg-accent'
-          >
-            <div className='flex items-center space-x-2'>
-              <div className='rounded-sm border p-1'>
-                <item.icon className='h-3 w-3' />
-              </div>
-              <span>{item.name}</span>
-            </div>
-            {activeItem.name === item.name && <Check className='h-4 w-4' />}
-          </EditorBubbleItem>
-        ))}
-      </PopoverContent>
-    </Popover>
+        <div className='flex items-center space-x-2'>
+          <div className='rounded-sm border p-1'>
+            <item.icon className='h-3 w-3' />
+          </div>
+          <span>{item.name}</span>
+        </div>
+        {activeItem.name === item.name && <Check className='h-4 w-4' />}
+      </EditorBubbleItem>
+    ))
+
+  const NodeSelectorTrigger = () => (
+    // className='gap-2 rounded-none border-none hover:bg-accent focus:ring-0'
+    <Button size='sm' variant='ghost' className='gap-2'>
+      <span className='whitespace-nowrap text-sm'>{activeItem.name}</span>
+      <ChevronDown className='h-4 w-4' />
+    </Button>
+  )
+
+  return (
+    <PopoverSelector
+      renderTrigger={NodeSelectorTrigger}
+      renderContent={NodeSelectorContent}
+      contentProps={{ className: 'w-48 p-1' }}
+    />
   )
 }
