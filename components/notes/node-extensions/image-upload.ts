76a140171c20f80254026d960917a74c5647d8ab
Alex <alexandru.pausan@gmail.com>
2024-08-04 16:03:35 +0300

    Add file upload functionality with image type validation and processing. Fix image upload size limit

--- components/notes/node-extensions/image-upload.ts
+++ components/notes/node-extensions/image-upload.ts
@@ -17,6 +17,7 @@ const onUpload = (file: File) => {
         // Successfully uploaded image
         if (res.status === 200) {
           const { url } = (await res.json()) as any
+          console.log(url, res)
           // preload the image
           let image = new Image()
           image.src = url
@@ -49,8 +50,9 @@ export const handleImageUpload = createImageUpload({
     if (!file.type.includes('image/')) {
       toast.error('File type not supported.')
       return false
-    } else if (file.size / 1024 / 1024 > 20) {
-      toast.error('File size too big (max 20MB).')
+    }
+    if (file.size / 1024 / 1024 > 10) {
+      toast.error('File size too big (max 10MB).')
       return false
     }
     return true
