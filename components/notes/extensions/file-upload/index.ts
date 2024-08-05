33f9d2620b4dd070f3b7fb6d54c471fbc27c7028
Alex <alexandru.pausan@gmail.com>
2024-08-05 17:32:03 +0300

    Refactor file-upload module to include FileUpload node with commands and NodeView

--- components/notes/extensions/file-upload/index.ts
+++ components/notes/extensions/file-upload/index.ts
@@ -1,1 +1,42 @@
-export * from './image-upload'
+import { Node, ReactNodeViewRenderer } from '@tiptap/react'
+import { FileUploadComponent } from './ui'
+
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    fileUpload: {
+      setFileUpload: () => ReturnType
+    }
+  }
+}
+
+export const FileUpload = Node.create({
+  name: 'fileUpload',
+
+  defining: true,
+  draggable: true,
+  group: 'block',
+  inline: false,
+  isolating: true,
+  selectable: true,
+
+  parseHTML() {
+    return [{ tag: `div[data-type="${this.name}"]` }]
+  },
+  renderHTML() {
+    return ['div', { 'data-type': this.name }]
+  },
+  addCommands() {
+    return {
+      setFileUpload:
+        () =>
+        ({ commands }) =>
+          commands.insertContent(`<div data-type="${this.name}"></div>`),
+    }
+  },
+
+  addNodeView() {
+    return ReactNodeViewRenderer(FileUploadComponent)
+  },
+})
+
+export default FileUpload
