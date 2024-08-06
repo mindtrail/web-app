5b8a8e2378e18e54ebe846c15f650a38794cb479
Alex <alexandru.pausan@gmail.com>
2024-08-06 12:57:59 +0300

    Improve image alignment UI in image viewer menu and add spacing to width input

--- components/notes/extensions/image-viewer/ui/menu.tsx
+++ components/notes/extensions/image-viewer/ui/menu.tsx
@@ -1,15 +1,16 @@
-import React, { useCallback, useRef, useState } from 'react'
-import { Instance } from 'tippy.js'
+import React, { useCallback } from 'react'
 import { EditorBubble, useEditor } from 'novel'
-import { AlignLeftIcon, AlignCenterIcon, AlignRightIcon } from 'lucide-react'
+import {
+  AlignHorizontalJustifyStartIcon,
+  AlignHorizontalJustifyCenterIcon,
+  AlignHorizontalJustifyEndIcon,
+} from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
-import { Separator } from '@/components/ui/separator'
 import { ImageViewerWidth } from './width'
 
 export const ImageViewerMenu = () => {
   const { editor } = useEditor()
-  const tippyInstance = useRef<Instance | null>(null)
 
   if (!editor) return null
 
@@ -19,27 +20,15 @@ export const ImageViewerMenu = () => {
   }, [editor])
 
   const onAlignImageLeft = useCallback(() => {
-    editor
-      .chain()
-      // .focus(undefined, { scrollIntoView: false })
-      .setImageViewerAlign('left')
-      .run()
+    editor.chain().setImageViewerAlign('left').run()
   }, [editor])
 
   const onAlignImageCenter = useCallback(() => {
-    editor
-      .chain()
-      // .focus(undefined, { scrollIntoView: false })
-      .setImageViewerAlign('center')
-      .run()
+    editor.chain().setImageViewerAlign('center').run()
   }, [editor])
 
   const onAlignImageRight = useCallback(() => {
-    editor
-      .chain()
-      // .focus(undefined, { scrollIntoView: false })
-      .setImageViewerAlign('right')
-      .run()
+    editor.chain().setImageViewerAlign('right').run()
   }, [editor])
 
   const onWidthChange = useCallback(
@@ -56,44 +45,35 @@ export const ImageViewerMenu = () => {
   return (
     <EditorBubble
       shouldShow={shouldShow}
-      tippyOptions={{
-        offset: [0, 8],
-        placement: 'top',
-        onHidden: () => {
-          editor.chain().unsetHighlight().run()
-        },
-        onCreate: (instance: Instance) => {
-          console.log(222, instance)
-          // tippyInstance.current = instance
-        },
-      }}
-      className='flex w-fit items-center overflow-hidden rounded-md border border-muted bg-background shadow-xl'
+      className='flex w-fit items-center overflow-hidden rounded-md border border-muted bg-background shadow-xl px-2 py-1 gap-2'
     >
-      <Button
-        onClick={onAlignImageLeft}
-        variant='ghost'
-        className={`rounded-none ${editor.isActive('imageViewer', { align: 'left' }) ? 'bg-muted' : ''}`}
-      >
-        <AlignLeftIcon className='h-4 w-4' />
-      </Button>
-      <Separator orientation='vertical' className='h-4' />
-      <Button
-        onClick={onAlignImageCenter}
-        variant='ghost'
-        className={`rounded-none ${editor.isActive('imageViewer', { align: 'center' }) ? 'bg-muted' : ''}`}
-      >
-        <AlignCenterIcon className='h-4 w-4' />
-      </Button>
-      <Separator orientation='vertical' className='h-4' />
-      <Button
-        onClick={onAlignImageRight}
-        variant='ghost'
-        className={`rounded-none ${editor.isActive('imageViewer', { align: 'right' }) ? 'bg-muted' : ''}`}
-      >
-        <AlignRightIcon className='h-4 w-4' />
-      </Button>
+      <div className='flex'>
+        <Button
+          onClick={onAlignImageLeft}
+          variant='ghost'
+          size='icon'
+          className={`${editor.isActive('imageViewer', { align: 'left' }) ? 'bg-muted' : ''}`}
+        >
+          <AlignHorizontalJustifyStartIcon className='h-4 w-4' />
+        </Button>
+        <Button
+          onClick={onAlignImageCenter}
+          variant='ghost'
+          size='icon'
+          className={`${editor.isActive('imageViewer', { align: 'center' }) ? 'bg-muted' : ''}`}
+        >
+          <AlignHorizontalJustifyCenterIcon className='h-4 w-4' />
+        </Button>
+        <Button
+          onClick={onAlignImageRight}
+          variant='ghost'
+          size='icon'
+          className={`${editor.isActive('imageViewer', { align: 'right' }) ? 'bg-muted' : ''}`}
+        >
+          <AlignHorizontalJustifyEndIcon className='h-4 w-4' />
+        </Button>
+      </div>
 
-      <Separator orientation='vertical' className='h-4' />
       <ImageViewerWidth
         onChange={onWidthChange}
         value={parseInt(editor.getAttributes('imageViewer').width || 0)}
