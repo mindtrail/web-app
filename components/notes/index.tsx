e21e73125fad0fc3ebd896181548e8a51e316417
Alex <alexandru.pausan@gmail.com>
2024-08-07 12:28:11 +0300

    Update notes component styles and headings in NotesComponent

--- components/notes/index.tsx
+++ components/notes/index.tsx
@@ -7,7 +7,7 @@ export function NotesComponent() {
   return (
     <div id='notes' className='flex flex-col flex-1 h-screen overflow-hidden'>
       <div className='py-8 px-16'>
-        <Typography variant='h1'>No code - Notebook</Typography>
+        <Typography variant='h2'>Scrib - No code Notebook</Typography>
         {/* <Typography
           variant='small'
           className='rounded-md px-1.5 py-0.5 bg-violet-50 text-violet-800'
@@ -17,7 +17,7 @@ export function NotesComponent() {
         </Typography> */}
       </div>
       <Separator />
-      <div className='py-8 px-16 overflow-auto'>
+      <div className='px-4 py-4 sm:px-8 md:py-8 md:px-16 overflow-auto'>
         <Editor />
       </div>
     </div>
